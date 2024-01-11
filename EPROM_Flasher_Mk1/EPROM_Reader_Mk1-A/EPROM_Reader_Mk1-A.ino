7a2b1648967dc1d3af898df8f3536bbfa437f227
Ashley <ashleydylanrudd@gmail.com>
2024-01-11 19:50:43 +0000

    Pulled Program from EPROM

--- EPROM_Flasher_Mk1/EPROM_Reader_Mk1-A/EPROM_Reader_Mk1-A.ino
+++ EPROM_Flasher_Mk1/EPROM_Reader_Mk1-A/EPROM_Reader_Mk1-A.ino
@@ -1,25 +1,50 @@
+/*******************************************************************************
+* This is an extremely basic EPROM Reader using an Arduino Mega, it reads each
+* byte, and prints it to the serial device as a raw binary byte. This is then
+* caught on the host PC by using
+*    (stty raw; cat > read.bin) < /dev/ttyACM0
+*
+* ADBeta (c) 11 Jan 2024
+*******************************************************************************/
 #include "turbo595.h"
 #include "chASM2.h"
+
+#include <string.h>
 
 #define DATA 11
 #define SCLK 12
 #define RCLK 13
 
-turbo595_t ADDR;
+turbo595_t addr_sr;
+#define MAX_ADDR 0x8000
+
+chasm_t data[8];
 
 void setup()
 {
+	Serial.begin(115200);
+
 	//Initialize the shift register
-	turbo595_init(&ADDR, SCLK, RCLK, DATA);
-
-	for(int x = 0; x < 255; x++) {
-		//Write 16bits to the Shift Register
-		turbo595_write_8(&ADDR, x);
-		
-		delay(100);
-	}
-
+	turbo595_init(&addr_sr, SCLK, RCLK, DATA);
 	
+	for(uint8_t pin = 0; pin < 8; pin++) {
+		chasm_init(&data[pin], 46 + pin);
+		chasm_setmode(&data[pin], INPUT);
+	}
+	
+	for(uint16_t addr = 0; addr < MAX_ADDR; addr++) {
+		//Write the address to the shift register
+		turbo595_write_16(&addr_sr, addr);
+		
+		//Get the data from the bus and storw it in the data_byte variable
+		uint8_t data_byte = 0;
+		for(uint8_t pin = 0; pin < 8; pin++) {
+			uint8_t bit = chasm_read(&data[pin]);
+			
+			data_byte |= bit << pin;
+		}
+		Serial.write(data_byte);	
+	}
 }
 
 void loop() 
---
+++ Software/README.md
@@ -0,0 +1,9 @@
+# Z80 Lotto Board - Software
+
+The [original software](https://github.com/ADBeta/Z80_Lotto_Board/blob/main/Software/original_rom.bin)
+has been dumped from the UV EPROM.
+
+The ASCII strings inside this ROM have also been [extracted seperately](https://github.com/ADBeta/Z80_Lotto_Board/blob/main/Software/original_rom_strings.txt)
+as they contain extra information about the board and original usecase.
+
+(TODO) The code will be reverse engineered soon
